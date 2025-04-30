9d8a03c6144bde99f5fe148898bb86287b0ff234
sufiON <as.abusufian2005@gmail.com>
2025-04-30 17:39:59 +0600

    First one

---
+++ no : 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main ()
+{
+
+int a = 67;
+int b= 343;
+int jogfol = a + b;
+
+std::cout << a << '\n';
+std::cout << b << '\n';
+std::cout << jogfol << '\n';
+
+return 0;
+
+
+
+
+
+
+}
