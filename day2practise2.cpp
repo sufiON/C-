2183b61bf83e9ba76cdd80625b680b6fdbb415a8
sufiON <as.abusufian2005@gmail.com>
2025-05-03 03:12:59 +0600

    3

---
+++ day2practise2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ int main()
+
+{
+std::cout << " ami bangla lekha likhte pari bro, chill " << std::endl;
+
+std::cout << " ami bangla porte pari 2.p" << std::endl;
+
+return 0;
+
+
+}
